1c0de8d6b0da1fc3404cbb22271b61e825c6c7e5
Peter Briggs <peter.briggs@manchester.ac.uk>
2015-07-03 12:07:19 +0100

    Update setup.py for new configuration.

--- setup.py
+++ setup.py
@@ -2,38 +2,43 @@
 
 Setup script to install RnaChipIntegrator: analyses of ChIP-Seq and RNA-Seq data
 
-Copyright (C) University of Manchester 2011-12 Peter Briggs, Leo Zeef & Ian Donaldson
+Copyright (C) University of Manchester 2011-15 Peter Briggs, Leo Zeef &
+Ian Donaldson
 
-This code is free software; you can redistribute it and/or modify it
-under the terms of the Artistic License 2.0 (see the file LICENSE included
-with the distribution).
-
-This will install RnaChipIntegrator and its dependencies and provide the
-'RnaChipIntegrator' command.
 """
 
+# Hack to acquire all scripts that we want to
+# install into 'bin'
+from glob import glob
+scripts = []
+for pattern in ('bin/*.py',):
+    scripts.extend(glob(pattern))
+
+# Setup for installation etc
 from setuptools import setup
-import RnaChipIntegrator
+import rnachipintegrator
 setup(
     name = "RnaChipIntegrator",
-    version = RnaChipIntegrator.__version__,
+    version = rnachipintegrator.get_version(),
     maintainer = 'Peter Briggs',
     maintainer_email = 'peter.briggs@manchester.ac.uk',
-    license = 'Artistic License 2.0',
+    packages = ['rnachipintegrator'],
+    license = 'Artistic License',
     py_modules = ['RnaChipIntegrator','Spreadsheet'],
     install_requires = ['xlwt >= 0.7.2',
                         'xlrd >= 0.7.1',
                         'xlutils >= 1.4.1'],
-    entry_points = { 'console_scripts':
-                         ['RnaChipIntegrator = RnaChipIntegrator:main',
-                          'RnaChipIntegrator.py = RnaChipIntegrator:main']
-                     },
-    data_files = [ ('RnaChipIntegrator', ['README.markdown',
-                                          'LICENSE',
-                                          'INSTALL',
-                                          'ChangeLog']),
-                   ('RnaChipIntegrator/doc', ['doc/MANUAL.markdown',
-                                              'doc/rnachipintegrator_nearestEdgetoPeak.png',
-                                              'doc/rnachipintegrator_nearestTSStoSummit.png',
-                                              'doc/rnachipintegrator_nearestTSStoPeak.png']) ]
+    test_suite = 'nose.collector',
+    tests_require = ['nose'],
+    scripts = scripts,
+    data_files = [ ('RnaChipIntegrator',
+                    ['README.markdown',
+                     'LICENSE',
+                     'INSTALL',
+                     'ChangeLog']),
+                   ('RnaChipIntegrator/doc',
+                    ['doc/MANUAL.markdown',
+                     'doc/rnachipintegrator_nearestEdgetoPeak.png',
+                     'doc/rnachipintegrator_nearestTSStoSummit.png',
+                     'doc/rnachipintegrator_nearestTSStoPeak.png']) ]
 )
