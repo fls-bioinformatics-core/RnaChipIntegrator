bf0c9afde1e46c700fc7b8caa4aaf4421e5e76b7
Peter Briggs <peter.briggs@manchester.ac.uk>
2015-10-27 14:21:02 +0000

    Update test script and reference outputs.

--- examples/run_examples.sh
+++ examples/run_examples.sh
@@ -12,17 +12,17 @@ fi
 cd test-output
 #
 # Summits
-SUMMIT_OUTPUTS="summits_PeaksToTranscripts.txt summits_TSSToSummits.txt"
-RnaChipIntegrator.py --project=summits \
-    --window=130000 \
+SUMMIT_OUTPUTS="summits_features_per_peak.txt summits_peaks_per_feature.txt"
+RnaChipIntegrator --name=summits \
+    --cutoff=130000 \
     $TEST_DIR/ExpressionData.txt \
     $TEST_DIR/ChIP_summits.txt
 for f in $SUMMIT_OUTPUTS ; do
     diff -f $REF_DATA/ref_$f $f
 done
 # Regions
-REGION_OUTPUTS="regions_TranscriptsToPeakEdges_summary.txt regions_TranscriptsToPeakEdges.txt regions_TSSToPeakEdges_summary.txt regions_TSSToPeakEdges.txt"
-RnaChipIntegrator.py --project=regions \
+REGION_OUTPUTS="regions_features_per_peak.txt regions_peaks_per_feature.txt"
+RnaChipIntegrator --name=regions \
     $TEST_DIR/ExpressionData.txt \
     $TEST_DIR/ChIP_regions.txt
 for f in $REGION_OUTPUTS ; do
