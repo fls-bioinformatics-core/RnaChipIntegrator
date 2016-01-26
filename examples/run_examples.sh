566196da25b119270c674fc36e320124d4177e65
Peter Briggs <peter.briggs@manchester.ac.uk>
2016-01-26 16:09:40 +0000

    Update the examples to check renamed output files (issue #25).

--- examples/run_examples.sh
+++ examples/run_examples.sh
@@ -12,7 +12,7 @@ fi
 cd test-output
 #
 # Summits
-SUMMIT_OUTPUTS="summits_features_per_peak.txt summits_peaks_per_feature.txt"
+SUMMIT_OUTPUTS="summits_peak_centric.txt summits_gene_centric.txt"
 RnaChipIntegrator --name=summits \
     --cutoff=130000 \
     $TEST_DIR/ExpressionData.txt \
@@ -26,7 +26,7 @@ for f in $SUMMIT_OUTPUTS ; do
     fi
 done
 # Regions
-REGION_OUTPUTS="regions_features_per_peak.txt regions_peaks_per_feature.txt"
+REGION_OUTPUTS="regions_peak_centric.txt regions_gene_centric.txt"
 RnaChipIntegrator --name=regions \
     $TEST_DIR/ExpressionData.txt \
     $TEST_DIR/ChIP_regions.txt
