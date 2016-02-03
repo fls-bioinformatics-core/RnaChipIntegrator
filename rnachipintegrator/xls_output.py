5e830d43635f0c921263cfb12d0a524e29f7050a
Peter Briggs <peter.briggs@manchester.ac.uk>
2016-02-03 11:13:33 +0000

    Reimplement spreadsheet writing to generate XLSX files (issue #38).
    This includes renaming the --xls option to --xlsx; removing the
    Spreadsheet.py module and the dependency on xlwt, xlrd and xlutils;
    adding a new dependency on xlsxwriter.

--- rnachipintegrator/xls_output.py
+++ rnachipintegrator/xls_output.py
@@ -1,22 +1,27 @@
 #!/bin/env python
 #
 #     xls_output.py: functions for writing analysis results to Excel files
-#     Copyright (C) University of Manchester 2015 Peter Briggs, Leo Zeef
+#     Copyright (C) University of Manchester 2015-16 Peter Briggs, Leo Zeef
 #     & Ian Donaldson
 #
 """
 xls_output.py
 
-Functions for outputting analysis results to XLS spreadsheet
+Functions for outputting analysis results to XLSX spreadsheet
 
 """
 import datetime
-import Spreadsheet
+import xlsxwriter
+import re
 import output
 import utils
 
+# Regular expressions for styling tags
+RE_STYLE = re.compile(r"^<style +([^>]*)>(.*)</style>$")
+
+# Notes text
 NOTES = dict()
-NOTES['preamble'] = """<style font=bold bgcolor=gray25>%s</style>
+NOTES['preamble'] = """<style font=bold bgcolor=gray>%s</style>
 
 Find nearest peaks to %ss (and vice versa)
 
@@ -24,33 +29,34 @@ Bioinformatics Core Facility, Faculty of Life Sciences, University of Manchester
 http://fls-bioinformatics-core.github.com/RnaChipIntegrator/
 Run on %s
 
-<style font=bold bgcolor=gray25>Settings</style>"""
+<style font=bold bgcolor=gray>Settings</style>"""
 NOTES['peak_centric'] = """
-<style font=bold bgcolor=gray25>'Peak-centric': nearest %ss to each peak</style>
+<style font=bold bgcolor=gray>'Peak-centric': nearest %ss to each peak</style>
 Column\tDescription"""
 NOTES['feature_centric'] = """
-<style font=bold bgcolor=gray25>'%s-centric': nearest peaks to each %s</style>
+<style font=bold bgcolor=gray>'%s-centric': nearest peaks to each %s</style>
 Column\tDescription"""
 
-class XLS:
+class XLSX:
     """
-    Class to assemble XLS output file
+    Class to assemble XLSX output file
 
-    Utility class to help build an XLS file from existing
+    Utility class to help build an XLSX file from existing
     output TSV files.
 
     Example usage:
 
-    >>> xls = XLS()
-    >>> xls.add_result_sheet('results','results.tsv')
-    >>> xls.write('results.xls')
+    >>> xlsx = XLS('results.xlsx')
+    >>> xlsx.add_result_sheet('results','results.tsv')
+    >>> xlsx.write()
 
     """
-    def __init__(self,program_version,feature_type=None):
+    def __init__(self,xlsx_file,program_version,feature_type=None):
         """
-        Create a new XLS instance
+        Create a new XLSX instance
 
         Arguments:
+          xlsx_file (str): name or path of output file
           program_version (str): name and version of the program
             that is writing the spreadsheet
           feature_type (str): if not None then replace 'feature'
@@ -58,16 +64,108 @@ class XLS:
             the output
 
         """
-        self._xls = Spreadsheet.Workbook()
-        self._title_limit = Spreadsheet.MAX_LEN_WORKSHEET_TITLE
-        self._char_limit = Spreadsheet.MAX_LEN_WORKSHEET_CELL_VALUE
-        self._line_limit = Spreadsheet.MAX_NUMBER_ROWS_PER_WORKSHEET
-        self._notes = self._xls.addSheet("Notes")
+        self._xlsx = xlsxwriter.Workbook(xlsx_file)
+        self._sheets = {}
+        self._rows = {}
+        self._widths = {}
+        self._styles = {}
         self._feature_type = ('gene' if feature_type is None
                               else feature_type)
-        self._notes.addText(NOTES['preamble'] % (program_version,
-                                                 self._feature_type,
-                                                 datetime.date.today()))
+        self.add_sheet("Notes")
+        self.append_to_notes(NOTES['preamble'] % (program_version,
+                                                  self._feature_type,
+                                                  datetime.date.today()))
+
+    def add_sheet(self,name):
+        """
+        Create a new worksheet in the XLSX file
+
+        Arguments:
+          name (str): title for the new sheet (must be
+            unique across the XLSX file)
+
+        Returns:
+          WorkSheet: new worksheet.
+
+        """
+        if name in self._sheets:
+            raise KeyError("'%s': worksheet already exists")
+        ws = self._xlsx.add_worksheet(name)
+        self._sheets[name] = ws
+        self._rows[name] = 0
+        self._widths[name] = []
+        return ws
+
+    def get_format(self,*args):
+        """
+        Return a cell format object matching arguments
+
+        Returns a Format object matching the supplied
+        arguments, which should be strings of the form
+
+        'KEY=VALUE'
+
+        Formats are cached so there will be one Format
+        per unique set of key/value pairs.
+
+        """
+        # Create a name for this style
+        name = list(args)[:]
+        name.sort()
+        name = "_".join(name)
+        # See if it's already defined
+        if name not in self._styles:
+            # Create a new style (cell_format)
+            fmt = self._xlsx.add_format()
+            for style in args:
+                if style == "font=bold":
+                    fmt.set_bold(True)
+                elif style.startswith("bgcolor="):
+                    color = style.split('=')[1]
+                    fmt.set_bg_color(color)
+                else:
+                    raise NotImplementedError("%s: not implemented" %
+                                              style)
+            self._styles[name] = fmt
+        # Return the cell format for this style
+        return self._styles[name]
+
+    def add_text(self,name,text):
+        """
+        Add (append) arbitrary text to a worksheet
+
+        Arguments:
+          name (str): name of the worksheet
+          text (str): text that will be added to the
+            end of the worksheet
+
+        """
+        ws = self._sheets[name]
+        i = self._rows[name]
+        for line in text.split('\n'):
+            j = 0
+            for item in line.split('\t'):
+                # Check for styles
+                style_match = RE_STYLE.match(item)
+                if style_match:
+                    item = style_match.group(2)
+                    style = style_match.group(1).split()
+                    fmt = self.get_format(*style)
+                else:
+                    fmt = None
+                # Write the item
+                ws.write(i,j,item,fmt)
+                # Update the widths
+                try:
+                    self._widths[name][j] = max(self._widths[name][j],
+                                                len(item))
+                except IndexError:
+                    self._widths[name].append(len(item))
+                # Increment column counter
+                j += 1
+            # Increment row counter
+            i += 1
+        self._rows[name] = i
 
     def append_to_notes(self,text):
         """
@@ -78,11 +176,11 @@ class XLS:
             end of the notes.
 
         """
-        self._notes.addText(text)
+        self.add_text("Notes",text)
 
     def write_peak_centric(self,fields):
         """
-        Write details of the 'peak-centric' results to XLS notes
+        Write details of the 'peak-centric' results to XLSX notes
 
         Arguments:
           fields (list): list of fields in the output
@@ -94,7 +192,7 @@ class XLS:
 
     def write_feature_centric(self,fields):
         """
-        Write details of the 'feature-centric' results to XLS notes
+        Write details of the 'feature-centric' results to XLSX notes
 
         Arguments:
           fields (list): list of fields in the output
@@ -107,7 +205,7 @@ class XLS:
 
     def _field_descriptions(self,fields):
         """
-        Generate field (column) descriptions for XLS notes
+        Generate field (column) descriptions for XLSX notes
 
         Arguments:
           fields (list): list of fields to describe
@@ -139,32 +237,34 @@ class XLS:
           tsv_file (str): path to a tab-delimited file
 
         """
-        ws = self._xls.addSheet(title)
-        n = 0
+        ws = self.add_sheet(title)
         # Get header line
         with open(tsv_file,'r') as fp:
-            header = fp.readline().rstrip('\n')
-        with open(tsv_file,'r') as fp:
-            i = 0
+            i = self._rows[title]
             for line in fp:
-                if not i%self._line_limit and i:
-                    # Start a new sheet
-                    n += 1
-                    new_title = utils.truncate_text("%s(%d)" % (title,n),
-                                                    self._title_limit)
-                    print "Wrapping onto new sheet: %s" % new_title
-                    ws = self._xls.addSheet(new_title)
-                    ws.addText(header)
-                    i += 1
-                ws.addText(line.rstrip('\n'))
+                j = 0
+                for value in line.rstrip('\n').split('\t'):
+                    ws.write(i,j,value)
+                    try:
+                        self._widths[title][j] = max(self._widths[title][j],
+                                                     len(value))
+                    except IndexError:
+                        self._widths[title].append(len(value))
+                    j += 1
                 i += 1
+            self._rows[title] = i
+        # Freeze the header
+        ws.freeze_panes(1,0)
 
-    def write(self,xls_file):
+    def write(self):
         """
-        Write XLS to a file
-
-        Arguments:
-          xls_file (str): name or path of output file
+        Write XLSX to file
 
         """
-        self._xls.save(xls_file)
+        # Set the column widths
+        for name in self._sheets:
+            ws = self._sheets[name]
+            for j,w in enumerate(self._widths[name]):
+                ws.set_column(j,j,w*1.2)
+        # Close to write to file
+        self._xlsx.close()
