d9a1118dc616be90d11828a21f13f2a7a5270f24
Peter Briggs <peter.briggs@manchester.ac.uk>
2018-01-22 15:17:06 +0000

    Update the copyright year range in the documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'RnaChipIntegrator'
-copyright = u'2015-2016, Peter Briggs, Ian Donaldson and Leo Zeef (University of Manchester)'
+copyright = u'2015-2018, Peter Briggs, Ian Donaldson and Leo Zeef (University of Manchester)'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
